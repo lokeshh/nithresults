d97bd4f7679467ba1e2f1e517ec947c119f5b8b4
Lokesh Sharma <lokeshhsharma@gmail.com>
2015-12-14 17:06:26 +0000

    Made cgpi order descending

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,9 @@
 class StudentsController < ApplicationController
   def index
-    @students = Student.order(params[:sort_by])
+    if params[:sort_by] == 'cgpi'
+      @students = Student.order(cgpi: :desc)
+    else
+      @students = Student.order(params[:sort_by])
+    end
   end
 end
